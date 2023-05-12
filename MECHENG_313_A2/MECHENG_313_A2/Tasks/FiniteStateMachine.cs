76e183b1d5badbe633c45943e546c079cee95335
thraasymaachus <unicycles431@gmail.com>
2023-05-12 15:23:37 +1200

    Implement SetNextState, and fix an error in that function and AddAction where updated information was given to a copy, rather than the true var

--- MECHENG_313_A2/MECHENG_313_A2/Tasks/FiniteStateMachine.cs
+++ MECHENG_313_A2/MECHENG_313_A2/Tasks/FiniteStateMachine.cs
@@ -30,12 +30,15 @@ namespace MECHENG_313_A2.Tasks
                 fst.Add(state, new Dictionary<string, StateInformation>()); // If it doesn't, add the row
             }
 
-            if (!fst[state].ContainsKey(eventTrigger)) // Check if the FST doesn't yet have the event within the state, add the column. If the FST previously had no state, this will necessarily be true.
+            if (!fst[state].ContainsKey(eventTrigger)) // Check if the FST doesn't yet have the event within the state. If the FST previously had no state, this will necessarily be true.
             {
-                fst[state].Add(eventTrigger, new StateInformation(state, new List<TimestampedAction>())); // If it doesn't, create a new state/event at the state
+                fst[state].Add(eventTrigger, new StateInformation("", new List<TimestampedAction>())); // If it doesn't, add the column. Note that the default value for nextState is ""
             }
 
-            fst[state][eventTrigger].actions.Add(action);
+
+            StateInformation updatedStateInfo = fst[state][eventTrigger]; // Need to use a temp struct, otherwise we will be working on a copy instead of the actual variable
+            updatedStateInfo.actions.Add(action);
+            fst[state][eventTrigger] = updatedStateInfo; // Add the action to the list of actions associated with the action/event
         }
 
         public string GetCurrentState()
@@ -51,12 +54,24 @@ namespace MECHENG_313_A2.Tasks
 
         public void SetCurrentState(string state)
         {
-            // TODO: Implement this
+            currentState = state;
         }
 
         public void SetNextState(string state, string nextState, string eventTrigger)
         {
-            // TODO: Implement this
+            if (!fst.ContainsKey(state)) // Check if the FST has the state
+            {
+                fst.Add(state, new Dictionary<string, StateInformation>()); // If it doesn't, add the row
+            }
+
+            if (!fst[state].ContainsKey(eventTrigger)) // Check if the FST doesn't yet have the event within the state. If the FST previously had no state, this will necessarily be true.
+            {
+                fst[state].Add(eventTrigger, new StateInformation("", new List<TimestampedAction>())); // If it doesn't, add the column
+            }
+
+            StateInformation updatedStateInfo = fst[state][eventTrigger]; // Need to use a temp struct, otherwise we will be working on a copy instead of the actual variable
+            updatedStateInfo.next = nextState;
+            fst[state][eventTrigger] = updatedStateInfo; // Set the next state string associated with the action/event
         }
     }
 }
