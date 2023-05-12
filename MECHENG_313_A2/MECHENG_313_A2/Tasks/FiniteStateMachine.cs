b661279a024030279f7f33771300462b34582eb8
thraasymaachus <unicycles431@gmail.com>
2023-05-12 15:00:50 +1200

    Add AddAction implementation

--- MECHENG_313_A2/MECHENG_313_A2/Tasks/FiniteStateMachine.cs
+++ MECHENG_313_A2/MECHENG_313_A2/Tasks/FiniteStateMachine.cs
@@ -9,23 +9,33 @@ namespace MECHENG_313_A2.Tasks
     public class FiniteStateMachine : IFiniteStateMachine
     {
         private string currentState;
-        private Dictionary<string, Dictionary<string, StateInformation>> fsm = new Dictionary<string, Dictionary<string, StateInformation>>();
+        private Dictionary<string, Dictionary<string, StateInformation>> fst = new Dictionary<string, Dictionary<string, StateInformation>>();
 
-        public struct StateInformation
+        public struct StateInformation // Define a struct to represent our state/events
         {
             public string next;
-            TimestampedAction[] actions;
+            public List<TimestampedAction> actions;
 
-            public StateInformation(string nextState, TimestampedAction[] actionsList)
+            public StateInformation(string nextState, List<TimestampedAction> actionsList)
             {
                 next = nextState;
                 actions = actionsList;
             }
         }
 
-        public void AddAction(string state, string eventTrigger, TimestampedAction action)
+        public void AddAction(string state, string eventTrigger, TimestampedAction action) // Implement 
         {
-            // TODO: Implement this
+            if (!fst.ContainsKey(state)) // Check if the FST has the state
+            {
+                fst.Add(state, new Dictionary<string, StateInformation>()); // If it doesn't, add the row
+            }
+
+            if (!fst[state].ContainsKey(eventTrigger)) // Check if the FST doesn't yet have the event within the state, add the column. If the FST previously had no state, this will necessarily be true.
+            {
+                fst[state].Add(eventTrigger, new StateInformation(state, new List<TimestampedAction>())); // If it doesn't, create a new state/event at the state
+            }
+
+            fst[state][eventTrigger].actions.Add(action);
         }
 
         public string GetCurrentState()
--- MECHENG_313_A2/MECHENG_313_A2/Tasks/Task2.cs
+++ MECHENG_313_A2/MECHENG_313_A2/Tasks/Task2.cs
@@ -10,9 +10,7 @@ namespace MECHENG_313_A2.Tasks
     internal class Task2 : IController
     {
         public virtual TaskNumber TaskNumber => TaskNumber.Task2;
-
         protected ITaskPage _taskPage;
-
         public void ConfigLightLength(int redLength, int greenLength)
         {
             // TODO: Implement this
@@ -24,53 +22,48 @@ namespace MECHENG_313_A2.Tasks
             // TODO: Implement this
             return false;
         }
-
         public void ExitConfigMode()
         {
             // TODO: Implement this
         }
-
         public async Task<string[]> GetPortNames()
         {
             // TODO: Implement this
-
-        };
-
-
-        public async Task<string> OpenLogFile()
-        {
-            // TODO: Implement this
-
-            // Help notes: to read a file named "log.txt" under the LocalApplicationData directory,
-            // you may use the following code snippet:
-            // string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "log.txt");
-            // string text = File.ReadAllText(filePath);
-            //
-            // You can also create/write to file(s) through System.IO.File. 
-            // See https://learn.microsoft.com/en-us/xamarin/xamarin-forms/data-cloud/data/files?tabs=windows, and
-            // https://learn.microsoft.com/en-us/dotnet/api/system.io.file?view=netstandard-2.0 for more details.
-            return null;
+            return new string[0];
         }
 
-        public async Task<bool> OpenPort(string serialPort, int baudRate)
-        {
-            // TODO: Implement this
-            return false;
-        }
+    };
 
-        public void RegisterTaskPage(ITaskPage taskPage)
-        {
-            _taskPage = taskPage;
-        }
 
-        public void Start()
-        {
-            // TODO: Implement this
-        }
-
-        public void Tick()
-        {
-            // TODO: Implement this
-        }
+    public async Task<string> OpenLogFile()
+    {
+        // TODO: Implement this
+        // Help notes: to read a file named "log.txt" under the LocalApplicationData directory,
+        // you may use the following code snippet:
+        // string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "log.txt");
+        // string text = File.ReadAllText(filePath);
+        //
+        // You can also create/write to file(s) through System.IO.File. 
+        // See https://learn.microsoft.com/en-us/xamarin/xamarin-forms/data-cloud/data/files?tabs=windows, and
+        // https://learn.microsoft.com/en-us/dotnet/api/system.io.file?view=netstandard-2.0 for more details.
+        return null;
     }
+    public async Task<bool> OpenPort(string serialPort, int baudRate)
+    {
+        // TODO: Implement this
+        return false;
+    }
+    public void RegisterTaskPage(ITaskPage taskPage)
+    {
+        _taskPage = taskPage;
+    }
+    public void Start()
+    {
+        // TODO: Implement this
+    }
+    public void Tick()
+    {
+        // TODO: Implement this
+    }
+}
 }
